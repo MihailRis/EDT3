200fd7f0d3c8c6ab8a159d5fbb1f3ba2d11a05e1
MihailRis <mexaruc@gmail.com>
2022-08-28 00:07:25 +0300

    Added EDTWriteable.asEDT

--- src/main/java/mihailris/edtfile/EDTWriteable.java
+++ src/main/java/mihailris/edtfile/EDTWriteable.java
@@ -3,7 +3,13 @@ package mihailris.edtfile;
 public interface EDTWriteable {
     void write(EDTGroup root);
 
-    default byte[] asEdtBytes(String tag, boolean compression){
+    default EDTGroup asEDT(String tag){
+        EDTGroup root = EDTGroup.create(tag);
+        write(root);
+        return root;
+    }
+
+    default byte[] asEDTBytes(String tag, boolean compression){
         EDTGroup root = EDTGroup.create(tag);
         write(root);
         return EDT.write(root, compression);
